6b24f4b16e8e2d71b63b7cef3df8c324b7a1ed91
Alexander Roberts <remidy@live.co.uk>
2020-10-25 23:02:48 +0000

    Added toast messages, notification css and shopping bag preview

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -8,20 +11,32 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of a specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    bag[item_id] = quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
 def remove_from_bag(request, item_id):
     """ Remove a specific product from the shopping bag """
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
+
     bag.pop(item_id)
+    messages.success(request, f'Removed {product.name} from your bag')
+
     request.session['bag'] = bag
     return redirect('view_bag')
+    
