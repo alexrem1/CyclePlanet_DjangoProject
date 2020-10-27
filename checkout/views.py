e0949b41cc09e009b28f328a4f8866b88a99874a
Alexander Roberts <remidy@live.co.uk>
2020-10-27 11:43:37 +0000

    Added order summary to checkout success page

--- checkout/views.py
+++ checkout/views.py
@@ -84,9 +84,6 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Your order has been successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
