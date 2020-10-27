c34e232bcd0c9fb5b61bacbd50c880fe34375acb
Alexander Roberts <remidy@live.co.uk>
2020-10-27 12:38:51 +0000

    added webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -50,6 +50,7 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -65,6 +66,7 @@ form.addEventListener('submit', function(ev) {
             $(errorDiv).html(html);
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
+            
         } else {
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
