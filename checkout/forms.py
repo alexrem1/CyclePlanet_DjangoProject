b19ed52652b7ce40c82126e0a91b02ffad42c419
Alexander Roberts <remidy@live.co.uk>
2020-10-28 06:55:53 +0000

    Added profile form, views and updated template

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,11 +25,11 @@ class OrderForm(forms.ModelForm):
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County, State or Locality',
+            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'country':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
                 else:
